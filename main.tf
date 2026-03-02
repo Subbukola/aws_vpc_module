ca0c2ff625f45415f29dc03d89399be7be42f92e
subbu <subbu@example.com>
2026-03-02 23:19:18 +0530

    vpc

--- main.tf
+++ main.tf
@@ -145,6 +145,6 @@ resource "aws_nat_gateway" "nat" {
 resource "aws_route" "private_route" {
   route_table_id            = aws_route_table.private_route.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id=aws_nat_gateway.nat
+  gateway_id=aws_nat_gateway.nat.id
 }
 
