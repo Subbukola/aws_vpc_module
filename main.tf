945cfe13338c0414182cc342d31642e434e1c130
Kola Venkata SubrahmanyeswaraRao <ko20276757@wipro.com>
2026-03-02 17:44:32 +0530

    vpc

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ resource "aws_internet_gateway" "gw" {
 
 #Public_subnets
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.public.id
   count = length(var.public_cidr)
   cidr_block = var.public_cidr[count.index]
   availability_zone=local.az_names[count.index]
@@ -35,5 +35,45 @@ resource "aws_subnet" "main" {
         Name="${var.project}-${var.env}-public-${local.az_names[count.index]}"
     },
     var.public_subnet_tags
+  )
+}
+
+#Private_subnets
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.private.id
+  count = length(var.public_cidr)
+  cidr_block = var.public_cidr[count.index]
+  availability_zone=local.az_names[count.index]
+   map_public_ip_on_launch= true
+
+
+  tags = merge(
+
+    local.common_tags,
+    {
+        Name="${var.project}-${var.env}-public-${local.az_names[count.index]}"
+    },
+    var.public_subnet_tags
+  )
+}
+
+
+
+#Database_subnets
+
+resource "aws_subnet" "database" {
+  vpc_id     = aws_vpc.database.id
+  count = length(var.database_cidr)
+  cidr_block = var.database_cidr[count.index]
+  availability_zone=local.az_names[count.index]
+
+  tags = merge(
+
+    local.common_tags,
+    {
+        Name="${var.project}-${var.env}-database-${local.az_names[count.index]}"
+    },
+    var.database_subnet_tags
   )
 }
--- var.tf
+++ var.tf
@@ -36,3 +36,28 @@ variable "public_subnet_tags" {
     default = { }
   
 }
+
+variable "private_cidr" {
+  
+  type = list(string)
+  default = ["10.0.11.0/24","10.0.22.0/24"]
+}
+
+variable "private_subnet_tags" {
+    type = map(string)
+    default = { }
+  
+}
+
+variable "database_cidr" {
+
+  type = list(string)
+  default = ["10.0.31.0/24","10.0.32.0/24"]
+  
+}
+
+variable "database_subnet_tags" {
+    type = map(string)
+    default = { }
+  
+}
