c8042d938ef2d9395a962cc6d5b8e7f472cb289a
subbu <subbu@example.com>
2026-03-05 00:33:31 +0530

    vpc_peering

--- peering.tf
+++ peering.tf
@@ -14,3 +14,24 @@ resource "aws_vpc_peering_connection" "vpc_peering" {
 
 
 }   
+
+
+# #default vpc to myVPC
+# resource "aws_route" "route_to_my_vpc" {
+#   route_table_id            = aws_route_table.private_route
+#   destination_cidr_block    = data.aws_vpc.default_vpc.cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+# }
+
+# # myVPC to default VPC
+# resource "aws_route" "route_to_default_vpc" {
+#   route_table_id            = aws_route_table.private_route.id
+#   destination_cidr_block    = data.aws_vpc.default_vpc.cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+# }
+
+# resource "aws_route" "default_to_my_vpc" {
+#   route_table_id            = "default_vpc_route_table_id"
+#   destination_cidr_block    = aws_vpc.vpc.cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+# }
