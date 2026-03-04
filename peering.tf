40277bce0add98e5719845566fab9854675cb05b
subbu <subbu@example.com>
2026-03-04 22:44:14 +0530

    vpc_peering

--- peering.tf
+++ peering.tf
@@ -1,13 +1,13 @@
-resource "aws_vpc_peering_connection" "foo" {
+# resource "aws_vpc_peering_connection" "foo" {
 
-  #Accepter
-  peer_vpc_id   = aws_vpc.bar.id
+#   #Accepter
+#   peer_vpc_id   = aws_vpc.bar.id
 
-  #requester
-  vpc_id        = aws_vpc.foo.id
+#   #requester
+#   vpc_id        = aws_vpc.var.id
 
-  #accepting auto peer request
-  auto_accept   = true
-  
+#   #accepting auto peer request
+#   auto_accept   = true
 
-}   
+
+# }   
