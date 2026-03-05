3e63b8d0a730d36f1aaee788021c94e753b0ae42
subbu <subbu@example.com>
2026-03-05 16:55:37 +0530

    vpc-peering

--- peering.tf
+++ peering.tf
@@ -18,14 +18,14 @@ resource "aws_vpc_peering_connection" "vpc_peering" {
 
 #default vpc to myVPC
 resource "aws_route" "route_to_my_vpc" {
-  route_table_id            = data.aws_route_table.default.id
+  route_table_id            = data.aws_route_tables.default.id
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
 }
 
 # myVPC to default VPC
 resource "aws_route" "route_to_default_vpc" {
-  route_table_id            = aws_route_tables.private_route.id
+  route_table_id            = aws_route_table.private_route.id
   destination_cidr_block    = data.aws_vpc.default_vpc.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
 }
