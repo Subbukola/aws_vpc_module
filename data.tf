685f8e32f1f28382a26e2e66eb57692b35cb36d8
subbu <subbu@example.com>
2026-03-05 16:49:33 +0530

    vpc-peering

--- data.tf
+++ data.tf
@@ -10,3 +10,9 @@ data "aws_availability_zones" "available" {
 data "aws_vpc" "default_vpc" {
   default = true
 }
+
+data "aws_route_tables" "default" {
+  vpc_id = data.aws_vpc.default_vpc.id
+
+
+}
